d5c455eaa49c418d65c11471e6d5426e7fd3486e
Ntobeko <sitholent@gmail.com>
2022-06-01 09:03:09 -0700

    Add task #4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-/* this program prints using puts function */
-int main(void) 
+int main(void) /* this program prints using puts function*/
 {
 	/* string initialisation */
-	char Mystr[] = "\"Programming is like building a multilingual puzzle"
+	char Mystr[] = "\"Programming is like building a multilingual puzzle\n"
 
 	puts(Mystr); /* writing the string to stdout */
 	return (0);
