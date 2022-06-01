58cbd39b83c5866446c0205b83828a3db7602870
Ntobeko <sitholent@gmail.com>
2022-06-01 10:41:10 -0700

    Add task #5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ * printf - prints message
+ *return: 0
+*/
+int main(void)
+{
+  printf("with proper grammer%s but the outcome is a piece of art%s" ","",");
+  return (0);
+}
