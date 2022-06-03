3de548d7d55519715050c330bb86d4b7ea10f76a
Ntobeko <sitholent@gmail.com>
2022-06-03 14:14:25 -0700

    Add 6th task #5

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+*main - prints numbers
+*Return: 0
+*/
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		puthchar(1);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
