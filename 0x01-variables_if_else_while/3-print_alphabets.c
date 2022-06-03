6677e85610290ce69dc394830c97f3fc989bf476
Ntobeko <sitholent@gmail.com>
2022-06-03 13:31:47 -0700

    Add 4th task #3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+*main -entry point
+*Return:0
+*/
+int main(void)
+{
+	char a= 'a';
+
+	while (a <= 'Z')
+	{
+		putchar(a);
+		a++;
+	}
+
+	a = 'A';
+
+	while (a <= 'Z')
+	{
+		putchar(a);
+		a++;
+	}
+
+	putchar('\n');
+	return (0);
+}
