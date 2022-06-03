67fd0f61c6f07d6f934ac30a8afeea308189ab4e
Ntobeko <sitholent@gmail.com>
2022-06-03 14:34:27 -0700

    Add 8th task #7

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+*main - entry point
+*Return: 0
+*/
+
+int main(void)
+{
+	int 1;
+	for (i = 48; i < 48; i++)
+		putchar(1);
+	for (i = 'a'; i <= 'f'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
