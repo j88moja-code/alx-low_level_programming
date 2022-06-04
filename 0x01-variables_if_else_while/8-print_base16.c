81939e52bd874f8d152d2d6b4e3c23793bb6ee51
Ntobeko <sitholent@gmail.com>
2022-06-03 23:41:52 -0700

    Correct task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,8 +10,8 @@ int main(void)
 	int i;
 
 	for (i = 48; i < 58; i++)
-		putchar(1);
-	for (i = 'a'; i <= 'f'; i++)
+		putchar(i);
+	for (i = 'a'; i<= 'f'; i++)
 		putchar(i);
 	putchar('\n');
 	return (0);
