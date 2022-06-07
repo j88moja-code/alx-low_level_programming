5fa1dd357abd7bea6f9309977374c848d87b3b1c
Ntobeko <sitholent@gmail.com>
2022-06-07 06:26:04 -0700

    add task #1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,10 +3,7 @@
 /**
  *main -Entry point
  *
- * _putchar - writes the character c to stdout
- *c - character printed by the function _putchar
  *print_alphabet - function to print alphabet
- *Return: On success 1. 
  * Return - 0 (Success)
 */
 
