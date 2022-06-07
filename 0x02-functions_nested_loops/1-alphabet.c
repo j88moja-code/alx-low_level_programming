7d1a8165cff2e983dd8b58b7aca8efd4673e92d9
Ntobeko <sitholent@gmail.com>
2022-06-07 05:46:30 -0700

    Add task number 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *main -Entry point
+ *
+ * _putchar - writes the character c to stdout
+ *c - character printed by the function _putchar
+ *print_alphabet - function to print alphabet
+ *Return: On success 1. 
+ * Return - 0 (Success)
+*/
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,2 @@
 a program that prints _putchar, followed by a new line.
+a function that prints the alphabet, in lower case, followed by a new line
