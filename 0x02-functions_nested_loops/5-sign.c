2a2d701f9e849eb459300e940d818a15cda65167
Ntobeko <sitholent@gmail.com>
2022-06-07 10:45:02 -0700

    Add task #5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * main -Entry point
+ * print_sign -checks if the input integer n is the sign
+ *
+ * Description: function that prints the sign of a number.
+ * print_sign: return 1,0 and -1
+ * @n: the integer number being checked
+ * Return: 0 (Success)
+ */
+
+int print_sign(int n)
+{
+	if (n >= 0)
+	{
+		char a;
+
+		a = '+';
+		_putchar(a);
+		_putchar(',');
+		_putchar(' ');
+		_putchar(n + '0');
+		_putchar('\n');
+		return (1);
+	} else if (n <= 48)
+	{
+		char a;
+
+		a = '-';
+		_putchar(a);
+		_putchar(',');
+		_putchar(' ' );
+		_putchar(n + '0');
+		_putchar('\n');
+		return (-1);
+	} else if (n == 48)
+	{
+		char a;
+
+		a = '0';
+		_putchar(a);
+		_putchar(',');
+		_putchar(' ');
+		_putchar(n + '0');
+		_putchar('\n');
+		return (0);
+	} else
+		return (0);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -3,3 +3,4 @@
 #2 a function that prints 10 times the alphabet, in lowercase, followed by a new line.
 #3  a function that checks for lowercase character.
 #4 a function that checks for alphabetic character.The standard library provides a similar function: isalpha. Run man isalpha to learn more.
+#5 a function that prints the sign of a number
