64feb32d2ef23cc17119c8837b619f30ab3f3fab
Ntobeko <sitholent@gmail.com>
2022-06-07 04:02:52 -0700

    Add 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *main - Entry point
+ *
+ * _putchar - prints out message
+ * Return - 0 (Sucees)
+ */
+
+int main(void)
+{
+	char c[10];
+
+	c = "_putchar\n";
+	_putchar(c);
+
+	return (0);
+}
