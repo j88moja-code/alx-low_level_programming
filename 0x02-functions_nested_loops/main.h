154726e0e0f78264d3d375129a2d369eb8f27740
Ntobeko <sitholent@gmail.com>
2022-06-07 06:29:01 -0700

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,14 +2,8 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * print_char - prints alphabets
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+int _putchar(char c);
 
 void print_alphabet(void);
