ff0e2352e26a9a4962a84f2541d5b731062f961d
Ntobeko <sitholent@gmail.com>
2022-06-07 22:17:11 -0700

    Add task #15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	unsigned int a, b, c, d, sum;
+
+	a = 1;
+	b = 2;
+	c = 3;
+	sum = 2;
+
+	for (d = 2; d <= 32; d++)
+	{
+		if (c % 2 == 0)
+			sum = sum + c;
+		a = b;
+		b = c;
+		c = a + b;
+	}
+	printf("%u\n", s);
+	return (0);
+}
