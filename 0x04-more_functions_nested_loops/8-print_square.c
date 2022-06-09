64cee0fc11fe12cd27d577618102ed8d28dd361e
Ntobeko <sitholent@gmail.com>
2022-06-09 06:42:09 -0700

    Add task 8 #8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square.
+ *
+ * Description: a function that prints a square equal to size.
+ * @size: the size of the square.
+ */
+
+void print_square(int size)
+{
+	char c = 95;
+	int i = 0, j;
+
+	while (i < size)
+	{
+		j = 0;
+		while (j < size)
+		{
+			_putchar(c);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+	_putchar('\n');
+}
