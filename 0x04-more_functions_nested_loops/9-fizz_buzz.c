ee05fba63efa865b6e5c677c52903d5b63a7c69f
Ntobeko <sitholent@gmail.com>
2022-06-09 10:32:11 -0700

    Add task 9 #9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - checks the code.
+ *
+ * Description: a program that prints alternating strings and numbers.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+			printf("FizzBuzz\t");
+		else if ((i % 5) == 0)
+			printf("Buzz\t");
+		else if ((i % 3) == 0)
+			printf("Fizz\t");
+		else
+			printf("%d\t", i);
+	}
+	return (0);
+}
