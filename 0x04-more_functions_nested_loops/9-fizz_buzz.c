f99821365e93770ba7a101d2df7e28e4a12e2bc2
Ntobeko <sitholent@gmail.com>
2022-06-09 10:37:55 -0700

    Add task 9 #9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,9 +16,9 @@ int main(void)
 		if (i % 15 == 0)
 			printf("FizzBuzz\t");
 		else if ((i % 5) == 0)
-			printf("Buzz\t");
-		else if ((i % 3) == 0)
 			printf("Fizz\t");
+		else if ((i % 3) == 0)
+			printf("Buzz\t");
 		else
 			printf("%d\t", i);
 	}
