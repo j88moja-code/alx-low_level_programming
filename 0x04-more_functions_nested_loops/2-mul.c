c500ae59e48b9664e420f0a01d9abd872b76393c
Ntobeko <sitholent@gmail.com>
2022-06-09 03:28:28 -0700

    Add task 2 #2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers x and y.
+ *
+ * @x: the first integer.
+ * @y: the second integer.
+ * Description: a function that multiplies two integers.
+ * Return: result.
+ */
+
+int mul(int x, int y)
+{
+	int result = x * y;
+
+	return (result);
+}
