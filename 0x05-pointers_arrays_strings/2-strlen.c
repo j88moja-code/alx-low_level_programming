f9a9375bedfedacfac75ce7e4789cc6c90027bae
Ntobeko <sitholent@gmail.com>
2022-06-13 04:21:37 -0700

    Make it Betty Style aligned

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * _strlen - returns the length of a string.
- *
- * Return: length of a string.
- */
+* _strlen - returns the length of a string.
+* @s: a pointer for the string
+*
+* Return: length of a string.
+*/
 
 int _strlen(char *s)
 {
