cc39bf5e3c221afc8817ba0f9c66c3a67f3d5975
Ntobeko <sitholent@gmail.com>
2022-06-15 23:52:17 -0700

    Add task #4 4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses the content of an array of integers.
+ *
+ * @a: an array of integers.
+ * @n: the number of elements to swap.
+ * Return: nothing.
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, j;
+
+	for (i = 0, j = n - 1; i < j; i++, j--)
+	{
+		int temp = a[i];
+
+		a[i] = a[j];
+		a[j] = temp;
+	}
+}
