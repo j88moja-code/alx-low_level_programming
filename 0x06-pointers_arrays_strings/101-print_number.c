6919655b05ec48ebdab44ccce6b37857479f707d
Ntobeko <sitholent@gmail.com>
2022-06-16 12:28:33 -0700

    Add task #9 101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer.
+ *
+ * @n: an integer that runs through the program.
+ * Return: nothing.
+ */
+
+void print_number(int n)
+{
+	unsigned int i;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * - 1;
+	}
+	i = n;
+
+	if (i / 10)
+		print_number(i / 10);
+
+	_putchar(i % 10 + '0');
+}
