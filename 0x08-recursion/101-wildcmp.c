64a3188ddcff09812e3a410e2e9a706f18223ee3
Ntobeko <sitholent@gmail.com>
2022-06-23 02:22:45 -0700

    Modify task #8 101-wildcmp.c, alternative logic

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,34 +1,6 @@
 #include "main.h"
 
 /**
- * length - checks the length of a string.
- *
- * @s: the pointer to the string length need to be checked for.
- * Return: length of a string.
- */
-
-int length(char *s)
-{
-	if (*s != 0)
-		return (1 + (length(s + 1)));
-	return (0);
-}
-
-/**
- * from_end - checks total length of a string from end.
- * @s: the pointer to the string length need to be checked for.
- * @len: length of the string.
- * Return: length of the string.
- */
-
-char from_end(char *s, int len)
-{
-	if (*s != *(s + len))
-		return (from_end(s, len - 1));
-	return (*(s));
-}
-
-/**
  * wildcmp - compares two strings
  *
  * @s1: pointer to the first string to be compared.
@@ -38,41 +10,16 @@ char from_end(char *s, int len)
 
 int wildcmp(char *s1, char *s2)
 {
-	char *x = s2;
-	char *y = s1;
-	char *a = s1;
-	char *b = s2;
-
-	int len = length(y);
-
-	if ((*b == '*' && *a != 0) && *(a + 1) != *(b + 1))
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
 	{
-		return (wildcmp(++a, b));
-	}
-	else if ((*b == '*' && *a != 0) && *(a + 1) == *(b + 1))
-	{
-		return (wildcmp(++a, ++b));
-	}
-	else if ((*b == '*' && *a != 0))
-	{
-		if (*a == from_end(y, len))
-		{
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
+		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
 			return (1);
-		}
-		return ((wildcmp(a, ++b)));
 	}
-	else if ((*b == '*' && *a == 0) && *(b + 1) != 0 && *(b + 1) != '*')
-	{
-		if (*(a - 1) == from_end(x, length(x)))
-			return (1);
-		return (wildcmp(a, ++b));
-	}
-
-	if (*a != *b && *b != '*')
-		return (0);
-	if (*a == *b && *a != 0 && *b != 0)
-	{
-		return (wildcmp(++a, ++b));
-	}
-	return (1);
+	return (0);
 }
