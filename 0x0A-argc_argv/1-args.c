970297dbab86e74757aceb3313cea7f9af5cd4f3
Ntobeko <sitholent@gmail.com>
2022-06-24 06:51:37 -0700

    Add task #1 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - checks the code.
+ *
+ * @argc: arguments count.
+ * @argv: arguments vector.
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	(void)argv;
+
+	for (i = 0; i <= argc; i++)
+	{}
+	printf("%d\n", i - 2);
+
+	return (0);
+}
