c7bdfe05a90fe93c082cbc05a3343a50a2a44410
Ntobeko <sitholent@gmail.com>
2022-06-28 08:38:52 -0700

    Update task 3 3-alloc_grid.c logic

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,18 +12,24 @@
 int **alloc_grid(int width, int height)
 {
 	int **array;
-	int i;
+	int i, j;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
 	array = malloc(sizeof(int *) * height);
 	if (array == NULL)
+	{
+		free(array);
 		return (NULL);
-
+	}
 	for (i = 0; i < height; i++)
 	{
 		array[i] = malloc(sizeof(int) * width);
+		for (j = 0; j < width; j++)
+		{
+			array[i][j] = 0;
+		}
 	}
 	return (array);
 }
