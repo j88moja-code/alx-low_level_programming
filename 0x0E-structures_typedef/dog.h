d616cd4926b206a7cf884a501cf4099412bf7362
Ntobeko <sitholent@gmail.com>
2022-07-04 11:28:58 -0700

    Add first task 1-init_dog.c and update dog.h header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,9 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct dog - a struct that defines new dog.
  *
@@ -16,5 +19,7 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif /* DOG_H  */
