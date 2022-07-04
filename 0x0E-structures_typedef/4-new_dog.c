f3d1dad20e097fa29ec03571a7f246f64d44371c
Ntobeko <sitholent@gmail.com>
2022-07-04 12:48:37 -0700

    Correct forth task logic and make Betty compliant

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,7 +1,6 @@
 #include "dog.h"
 
 /**
- * dog_t - type def for creating new dog entry using struct dog.
  * new_dog - create a new dog entry.
  *
  * @name: name of the new dog entry.
@@ -13,30 +12,14 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int length_name, length_owner, x;
 	dog_t *dog;
 
-	length_name = length_owner = 0;
-	while (name[length_name++])
-		;
-	while (name[length_owner++])
-		;
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-
-	dog->name = malloc(length_name * sizeof(dog->name));
-	if (dog == NULL)
-		return (NULL);
-	for (x = 0; x < length_name; x++)
-		dog->name[x] = name[x];
+	dog->name = name;
 	dog->age = age;
-
-	dog->owner = malloc(length_owner * sizeof(dog->owner));
-	if (dog == NULL)
-		return (NULL);
-	for (x = 0; x < length_owner; x++)
-		dog->owner[x] = owner[x];
+	dog->owner = owner;
 
 	return (dog);
 }
