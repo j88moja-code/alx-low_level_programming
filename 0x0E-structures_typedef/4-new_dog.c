7b24ceeae986c193d2c2dfa4a0fa2b8abaa1005c
Ntobeko <sitholent@gmail.com>
2022-07-04 12:41:14 -0700

    Update logic for forth task in order to get rid of memory leaks

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,14 +13,30 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *buffer;
+	int length_name, length_owner, x;
+	dog_t *dog;
 
-	buffer = malloc(sizeof(dog_t));
-	if (buffer == NULL)
+	length_name = length_owner = 0;
+	while (name[length_name++])
+		;
+	while (name[length_owner++])
+		;
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
 		return (NULL);
-	buffer->name = name;
-	buffer->age = age;
-	buffer->owner = owner;
 
-	return (buffer);
+	dog->name = malloc(length_name * sizeof(dog->name));
+	if (dog == NULL)
+		return (NULL);
+	for (x = 0; x < length_name; x++)
+		dog->name[x] = name[x];
+	dog->age = age;
+
+	dog->owner = malloc(length_owner * sizeof(dog->owner));
+	if (dog == NULL)
+		return (NULL);
+	for (x = 0; x < length_owner; x++)
+		dog->owner[x] = owner[x];
+
+	return (dog);
 }
