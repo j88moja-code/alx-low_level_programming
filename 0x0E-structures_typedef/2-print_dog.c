a0b27ec27bd9173d0377004e511a6faeb5911b22
Ntobeko <sitholent@gmail.com>
2022-07-04 11:47:04 -0700

    Add second task 2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * print_dog - a function that prints out a struct dog.
+ *
+ * @d: pointer to the contents of the struct dog.
+ *
+ * Return: nothing.
+ */
+
+void print_dog(struct dog *d)
+{
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
