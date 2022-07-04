7cb4ac6059aef5d03a6fb212b643194fe7efb198
Ntobeko <sitholent@gmail.com>
2022-07-04 11:07:18 -0700

    Add zeroth task dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a struct that defines new dog.
+ *
+ * @name: pointer to the name of the dog.
+ * @age: the age of the dog.
+ * @owner: pointer to the name of the dog owner.
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H  */
