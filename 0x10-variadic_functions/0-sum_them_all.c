1422626802b01e22d1897540ab7cc6632cd5dda0
Ntobeko <sitholent@gmail.com>
2022-07-07 06:32:16 -0700

    Add zeroth task 0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - returns the sum of all  its parameters.
+ *
+ * @n: integers that are summed up.
+ *
+ * Return: sum or 0 for NULL.
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	int sum;
+	unsigned int i;
+
+	sum = 0;
+	va_start(ap, n);
+	if (n == 0)
+		return (0);
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, unsigned int);
+	va_end(ap);
+	return (sum);
+}
