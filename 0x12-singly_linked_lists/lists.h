435d681df875dc488f38b7d113c46afbdf908948
Ntobeko <sitholent@gmail.com>
2022-07-14 14:08:05 -0700

    Update header file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -28,4 +28,9 @@ size_t list_len(const list_t *h);
 
 list_t *add_node(list_t **head, const char *str);
 
+list_t *add_node_end(list **head, const char *str);
+
+void free_list(list_t *head);
+
+char *_strdup(const char *str);
 #endif /* LIST_H */
